dd886736cc0de9a949db5bad858262e5106a0e46
Chris Jones <cmjonescm@gmail.com>
2013-09-30 09:05:52 +0100

    Add hammer.js for touch control and tidy up code!

---
+++ app/app.js
@@ -0,0 +1,15 @@
+//*****************************************************************************
+// Copyright © 2013 Waters Corporation. All rights reserved.
+//*****************************************************************************
+'use strict';
+
+var app = angular.module('jogWheelApp', [ 'jogWheel.controller', 'jogWheelCtrl.directive']);
+
+
+
+app.config(['$provide', function($provide) {
+
+
+}]);
+
+
--- app/controllers/jogWheelController.js
+++ app/controllers/jogWheelController.js
@@ -3,43 +3,44 @@
 //*****************************************************************************
 
 
-jogWheelController = function($scope, $rootScope, $cacheFactory, $log, propertiesService) {
+angular.module('jogWheel.controller', [])
+    .controller('jogWheelController', ['$scope', function ($scope) {
+        // Controller functionality goes here
+/*
+        $scope.debug = "";
 
-    $scope.debug = "";
+        $scope.debugger = function(message) {
+            $scope.debug = message;
 
-    $scope.debugger = function(message) {
-       $scope.debug = message;
-
-       console.log("pps = " + message);
-    }
-
-
-    // public members
-
-    $scope.property = function(propertyName, setting) {
-       return propertiesService.property(propertyName, setting);
-    }
-
-    $scope.Increment = function(propertyName) {
-        propertiesService.Increment(propertyName);
-    }
-
-    $scope.Decrement = function(propertyName) {
-        propertiesService.Decrement(propertyName);
-    }
-
-    $scope.addProperty = function(name, attributes) {
-        propertiesService.addProperty(name, attributes);
-    }
-
-    $scope.IncrementBy = function(propertyName, setting) {
-        propertiesService.IncrementBy(propertyName, setting);
-    }  
+            console.log("pps = " + message);
+        };
 
 
-    // kick it all off
-    propertiesService.start();
+        // public members
 
-}
+        $scope.property = function(propertyName, setting) {
+            return propertiesService.property(propertyName, setting);
+        };
+
+        $scope.Increment = function(propertyName) {
+            propertiesService.Increment(propertyName);
+        };
+
+        $scope.Decrement = function(propertyName) {
+            propertiesService.Decrement(propertyName);
+        };
+
+        $scope.addProperty = function(name, attributes) {
+            propertiesService.addProperty(name, attributes);
+        };
+
+        $scope.IncrementBy = function(propertyName, setting) {
+            propertiesService.IncrementBy(propertyName, setting);
+        };
+
+*/
+
+
+    }]);
 
 
